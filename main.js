3c41b3df4ce510605d1b6d9fcd5bdc940118d10a
RishabhB07 <73791841+RishabhB07@users.noreply.github.com>
2021-09-09 20:16:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 function setup(){
     video=createCapture(VIDEO);
     video.size(550, 500);
-    canvas=createCanvas(550, 500);
-    canvas.position(560, 80);
+    canvas=createCanvas(550, 450);
+    canvas.position(560, 120);
     posenet=ml5.poseNet(video, modelLoaded);
     posenet.on("pose", gotresults);
 }
 function draw(){
     background("grey");
+    textSize(difference);
+    fill('#FFA500');
+    text('Hi', 10, 400);
 }
 function modelLoaded(){
     console.log("model loaded!");
 }
-function gotresults(result){
-    if(result.length>0){
-        console.log(result);
+difference=0;
+rightWristX=0;
+leftWristX=0;
+function gotresults(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftWristX="+leftWristX+"rightWristX="+rightWristX+"difference="+difference);
     }
 }
